3956b6bc62906e686f98874769db90b3234110ca
jasylwong <jasylwong@gmail.com>
2020-01-15 18:39:15 +0000

    First test added

---
+++ lib/fizzbuzz.rb
@@ -0,0 +1,5 @@
+def fizzbuzz(num)
+    "fizz"
+
+end
+
---
+++ spec/fizzbuzz_spec.rb
@@ -0,0 +1,7 @@
+require 'fizzbuzz'
+
+describe "Fizzbuzz" do
+    it 'should return "fizz" when you enter 3' do
+        expect(fizzbuzz(3)).to eq "fizz"
+    end
+end

2b658b0e1a2f4e9879541ff534086deed81cde9c
Ellie <elliemmanifold@gmail.com>
2020-01-15 18:51:19 +0000

    latest commit

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -4,4 +4,8 @@ describe "Fizzbuzz" do
     it 'should return "fizz" when you enter 3' do
         expect(fizzbuzz(3)).to eq "fizz"
     end
+
+    it 'should return "buzz" when you enter 5' do
+        expect(fizzbuzz(5)).to eq "buzz"
+    end
 end
